aaf9b69206d553dd8d0e8f5e0030a8a7880837f4
sunshineFlagma <andruschenkomax@gmail.com>
2025-04-03 21:26:54 +0300

    Alpha 1.0

---
+++ main.py
@@ -0,0 +1,78 @@
+import os, requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+def UserNameSearch(username):
+    global links, updated_links
+    links = ['https://www.instagram.com/', 'https://www.facebook.com/', 'https://tinder.com/','https://www.snapchat.com/add/',
+             'https://t.me/','https://www.x.com/','https://www.linkedin.com/in/','https://open.spotify.com/user/']
+    social_networks_names = ['Instagram','Facebook','Tinder','Snapchat','Telegram','X','LinkedIn','Spotify','Google']
+    google_search = f"https://www.google.com/search?q=%22{username}%22"
+    updated_links = [link + username for link in links]
+    print("Google - "+google_search)
+    for link, social_network in zip(updated_links, social_networks_names):
+        print(social_network.strip() + " - " + link.strip())
+
+def NumberPlateSearch(plate):
+    number_search = 'https://hotline.finance/ua/find-by-number/car?number=' + plate
+    os.system(f"start {number_search}")
+'''    url = str('https://hotline.finance/ua/find-by-number/car?number='+plate)
+    page = requests.get(url, headers=headers)
+    #print(page.text)
+    page.encoding = "utf-8"
+    soup = bs(page.text, "html.parser")
+    data = ['Name', 'Color', 'Plates', 'VIN' 'Type', 'Region', 'Fuel', 'First registration', 'Address']
+
+    car_name = [tag.text.strip() for tag in soup.findAll("h1", attrs={"class": "headerCarTitle__d747e7e2"})]
+    car_color = [tag.text.strip() for tag in soup.findAll("span", attrs={"class": "headerCarColor__e9de8a38"})]
+
+    car_info = [car_name,car_color]
+'''
+#anonymous
+print("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+ "⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠄⠄⠄⠁⠄⠁⠄⠄⠄⠄⠄\n"
+ "⠄⠄⠄⠄⠄⠄⣀⣀⣤⣤⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣦⣤⣤⣄⣀⡀⠄⠄⠄⠄⠄\n"
+ "⠄⠄⠄⠄⣴⣿⣿⡿⣿⢿⣟⣿⣻⣟⡿⣟⣿⣟⡿⣟⣿⣻⣟⣿⣻⢿⣻⡿⣿⢿⣷⣆⠄⠄⠄\n"
+"⠄⠄⠄⢘⣿⢯⣷⡿⡿⡿⢿⢿⣷⣯⡿⣽⣞⣷⣻⢯⣷⣻⣾⡿⡿⢿⢿⢿⢯⣟⣞⡮⡀⠄⠄\n"
+"⠄⠄⠄⢸⢞⠟⠃⣉⢉⠉⠉⠓⠫⢿⣿⣷⢷⣻⣞⣿⣾⡟⠽⠚⠊⠉⠉⠉⠙⠻⣞⢵⠂⠄⠄\n"
+"⠄⠄⠄⢜⢯⣺⢿⣻⣿⣿⣷⣔⡄⠄⠈⠛⣿⣿⡾⠋⠁⠄⠄⣄⣶⣾⣿⡿⣿⡳⡌⡗⡅⠄⠄\n"
+"⠄⠄⠄⢽⢱⢳⢹⡪⡞⠮⠯⢯⡻⡬⡐⢨⢿⣿⣿⢀⠐⡥⣻⡻⠯⡳⢳⢹⢜⢜⢜⢎⠆⠄⠄\n"
+"⠄⠄⠠⣻⢌⠘⠌⡂⠈⠁⠉⠁⠘⠑⢧⣕⣿⣿⣿⢤⡪⠚⠂⠈⠁⠁⠁⠂⡑⠡⡈⢮⠅⠄⠄\n"
+"⠄⠄⠠⣳⣿⣿⣽⣭⣶⣶⣶⣶⣶⣺⣟⣾⣻⣿⣯⢯⢿⣳⣶⣶⣶⣖⣶⣮⣭⣷⣽⣗⠍⠄⠄\n"
+'⠄⠄⢀⢻⡿⡿⣟⣿⣻⣽⣟⣿⢯⣟⣞⡷⣿⣿⣯⢿⢽⢯⣿⣻⣟⣿⣻⣟⣿⣻⢿⣿⢀⠄⠄\n'
+"⠄⠄⠄⡑⡏⠯⡯⡳⡯⣗⢯⢟⡽⣗⣯⣟⣿⣿⣾⣫⢿⣽⠾⡽⣺⢳⡫⡞⡗⡝⢕⠕⠄⠄⠄\n"
+"⠄⠄⠄⢂⡎⠅⡃⢇⠇⠇⣃⣧⡺⡻⡳⡫⣿⡿⣟⠞⠽⠯⢧⣅⣃⠣⠱⡑⡑⠨⢐⢌⠂⠄⠄\n"
+"⠄⠄⠄⠐⠼⣦⢀⠄⣶⣿⢿⣿⣧⣄⡌⠂⠢⠩⠂⠑⣁⣅⣾⢿⣟⣷⠦⠄⠄⡤⡇⡪⠄⠄⠄\n"
+"⠄⠄⠄⠄⠨⢻⣧⡅⡈⠛⠿⠿⠿⠛⠁⠄⢀⡀⠄⠄⠘⠻⠿⠿⠯⠓⠁⢠⣱⡿⢑⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠈⢌⢿⣷⡐⠤⣀⣀⣂⣀⢀⢀⡓⠝⡂⡀⢀⢀⢀⣀⣀⠤⢊⣼⡟⡡⡁⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠈⢢⠚⣿⣄⠈⠉⠛⠛⠟⠿⠿⠟⠿⠻⠻⠛⠛⠉⠄⣠⠾⢑⠰⠈⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠑⢌⠿⣦⡡⣱⣸⣸⣆⠄⠄⠄⣰⣕⢔⢔⠡⣼⠞⡡⠁⠁⠄⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠑⢝⢷⣕⡷⣿⡿⠄⠄⠠⣿⣯⣯⡳⡽⡋⠌⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢮⣿⣽⣯⠄⠄⢨⣿⣿⡷⡫⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠙⠝⠂⠄⢘⠋⠃⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄by MAXIMUS ⠄⠄⠄⠄⠄⠄⠄⠄⠄.⠄⠄⠄\n"
+"⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n")
+
+select_action = int(input("Select an action: \n1. Username search\n2. Number plate \n3. Check IP\n4. Exit\n"))
+
+if select_action == 1:
+    username = input("Input target username: ")
+    UserNameSearch(username)
+    all_links_open = str(input("Open all links? Y/n "))
+    if all_links_open == "Y":
+        for link in (updated_links):
+            os.system(f"start {link}")
+    else:
+        exit()
+elif select_action == 2:
+    plate = input("Input number plate: ")
+    NumberPlateSearch(plate)
+elif select_action == 3:
+    ip = input("Input targets IP: ")
+    ip_check_link = 'https://whatismyipaddress.com/ip/'
+    os.system(f"start {ip_check_link}"+ip)
+elif select_action == 4:
+    exit()
+else: exit()
